e5156d44cc7494332bc918fdff2d863b55745efa
Andrew_Rezk <andrew_rezk@yahoo.com>
2022-07-20 22:28:11 -0400

    added serverside api

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const qs = require("querystring"); //built-in querystring module for manipulating query strings
+const ejs = require("ejs");
+var fs = require('fs');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
-const {Client} = require("@googlemaps/google-maps-services-js");
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
-
-const client = new Client({});
+const port = process.env.PORT || "8080";
 
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-  res.render('index');
-})
+//set up static path (for use with CSS, client-side JS, and image files)
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post('/', function (req, res) {
-    res.render('index');
-  })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+//HTTP server listening
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  getPlaces(res);
+});
+
+
+function getPlaces(res){
+  const options = {
+    method: 'GET',
+    url: 'https://trueway-places.p.rapidapi.com/FindPlacesNearby',
+    params: {location: '49.2827,-123.1207', type: 'gym', radius: '200', language: 'en'},
+    headers: {
+      'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
+      'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
+    }
+  };
+  
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+    getPlaces(res);
+    res.render("index", {places: response.data})
+  }).catch(function (error) {
+    console.error(error);
+  });
+  }
