af3a15f9c9de6b819afaea93a2adf7894398cee1
Andrew_Rezk <andrew_rezk@yahoo.com>
2022-07-27 16:42:53 -0400

    fixed API calls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const axios = require("axios");
 const qs = require("querystring"); //built-in querystring module for manipulating query strings
 const ejs = require("ejs");
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -27,67 +27,91 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
+  // getGeo();
   getPlaces(res);
 });
 
+var lat;
+var lng;
 
 function getPlaces(res){
 
   const options = {
     method: 'GET',
     url: 'https://trueway-places.p.rapidapi.com/FindPlacesNearby',
-    params: {location: `37.7749,-122.402325`, type: 'cafe', radius: '150', language: 'en'},
+    params: {location: `${lat},${lng}`, type: 'cafe', radius: '150', language: 'en'},
     headers: {
       'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
       'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
     }
   };
   
-  
   axios.request(options).then(function (response) {
     console.log(response.data);
-      const options = {
-        method: 'GET',
-        url: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
-        params: {address: 'toronto, ontario', language: 'en'},
-        headers: {
-          'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
-          'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
-        }
-      };
-      axios.request(options).then(function (response) {
-        console.log(response.data);
-        lat = response.data.results.location.lat;
-        lng = response.data.results.location.lng;
-      }).catch(function (error) {
-        console.error(error);
-      });
     res.render("index", {places: response.data})
   }).catch(function (error) {
     console.error(error);
   });
+
+  const options2 = {
+    method: 'GET',
+    url: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
+    params: {address: 'toronto, ontario', language: 'en'},
+    headers: {
+      'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
+      'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options2).then(function (response) {
+    lat = response.data.results[0].location.lat;
+    lng = response.data.results[0].location.lng;
+    console.log(lat, lng);
+  })
+  
 }
 
+// function getData(res) { 
+//   var promiseArr = []
+//   var options = {
+//   method: 'GET',
+//   uri: 'https://trueway-places.p.rapidapi.com/FindPlacesNearby',
+//   params: {location: `${lat},${lng}`, type: 'cafe', radius: '150', language: 'en'},
+//   headers: {
+//     'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
+//     'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
+//     }
+//   };
+//   request(options).then(function(apires){
+//      console.log("complete 1");
+//      res.render("index", {places: response.data})
+//       var obj = JSON.parse(apires);
+//       obj.data.forEach(function(entry) {        
+//           var p = findMore(entry.id)
+//           promiseArr.push(p)
+//       });
+//   }).then(function(){
+//              Promise.all(promiseArr).then(function(){
+//              console.log("this is all done")
+//              })
+//           })
+// }
 
-  var lng;
-  var lat;
 
-  // function getGeo(res){
-  //   const options = {
-  //     method: 'GET',
-  //     url: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
-  //     params: {address: 'toronto, ontario', language: 'en'},
-  //     headers: {
-  //       'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
-  //       'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
-  //     }
-  //   };
-    
-  //   axios.request(options).then(function (response) {
-  //     console.log(response.data);
-  //     lat = response.data.results.location.lat;
-  //     lng = response.data.results.location.lng;
-  //   }).catch(function (error) {
-  //     console.error(error);
-  //   });
-  // }
+// function findMore(id) {
+//   var options = {
+//   method: 'GET',
+//   uri: 'https://trueway-geocoding.p.rapidapi.com/Geocode',
+//     params: {address: 'Cairo, Egypt', language: 'en'},
+//     headers: {
+//       'X-RapidAPI-Key': '49b3d82605msh88c216e62d3770cp13f60cjsnec91372acf2e',
+//       'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
+//     }
+//   };
+//   return request(options).then(function(apires){
+//      console.log("complete 2");
+//       var obj = JSON.parse(apires);
+//       lat = response.data.results[0].location.lat;
+//     lng = response.data.results[0].location.lng;
+//   })
+// }
